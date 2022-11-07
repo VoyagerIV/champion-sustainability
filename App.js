fa5187322615994cabdf28e782d15e6d93eba024
VoyagerIV <58806671+VoyagerIV@users.noreply.github.com>
2022-11-08 05:28:14 +1100

    Add categories selector

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Event from "./components/Event";
 import Comment from "./components/Comment";
+import Categories from './components/Categories';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Event
+      {/* <Event
         image="https://olympus-imaging.com.au/content/000107507.jpg"
         title="Event title"
         organiser="susansmith" // unique username
@@ -23,7 +24,8 @@ export default function App() {
         posterImg="https://thebluegrasssituation.com/wp-content/uploads/2020/08/re-perez-branding-for-the-people-square-headshot.jpg"
         rating={2}
         comment="tincidunt rutrum. Fusce imperdiet pellentesque augue, faucibus commodo sem consequat sit amet.agna, acnar. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptipsum d"
-      />
+      /> */}
+      {/* <Categories /> */}
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/Categories.jsx
@@ -0,0 +1,147 @@
+import { useState } from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+
+const data = [
+  [
+    {
+      category: "Education",
+      image: require("../assets/book.png"),
+    },
+    {
+      category: "Climate",
+      image: require("../assets/sun.png"),
+    },
+    {
+      category: "Animal",
+      image: require("../assets/animal.png"),
+    },
+  ],
+  [
+    {
+      category: "Recycling",
+      image: require("../assets/recycling-bin.png"),
+    },
+    {
+      category: "Planting",
+      image: require("../assets/farming.png"),
+    },
+    {
+      category: "Water Polution",
+      image: require("../assets/water-cycle.png"),
+    },
+  ],
+  [
+    {
+      category: "Overfishing",
+      image: require("../assets/fishing.png"),
+    },
+    {
+      category: "Acidification",
+      image: require("../assets/acid-rain.png"),
+    },
+    {
+      category: "Air Pollution",
+      image: require("../assets/factory.png"),
+    },
+  ],
+  [
+    {
+      category: "Food Waste",
+      image: require("../assets/waste.png"),
+    },
+    {
+      category: "Land Degredation",
+      image: require("../assets/land.png"),
+    },
+    {
+      category: "Plastic Pollution",
+      image: require("../assets/plastic-recycling.png"),
+    },
+  ],
+];
+
+const Category = ({ category, image, selected, setSelected }) => {
+    return (
+      <TouchableOpacity 
+        style={
+          selected.includes(category) ? [styles.categoryContainer, styles.categorySelected] : styles.categoryContainer
+        }
+        activeOpacity={1}
+        onPress={() => {
+          if (selected.includes(category)) {
+            setSelected(selected.filter(item => item != category));
+          } else {
+            setSelected([...selected, category])
+          }
+        }}
+      >
+        <Image style={styles.icon} source={image} />
+        <Text style={styles.categoryText}>{category}</Text>
+      </TouchableOpacity>
+    );
+}
+
+const Categories = () => {
+  const [selected, setSelected] = useState([]);
+  
+  return (
+    <View style={styles.container}>
+      {
+        [...Array(4).keys()].map(row => {
+          return (
+            <View style={styles.row}>
+              {data[row].map((item, indx) => {
+                return (
+                  <Category
+                    key={indx}
+                    category={item.category}
+                    image={item.image}
+                    selected={selected}
+                    setSelected={setSelected}
+                  />
+                );
+              })}
+            </View>
+          );
+        })
+      }
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    paddingHorizontal: 25, // TODO: remove after padding is added to screen
+  },
+  row: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 15,
+  },
+  categoryContainer: {
+    width: "30%",
+    height: 110,
+    backgroundColor: "#DAF6E6",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 15,
+  },
+  categorySelected: {
+    backgroundColor: "#45C278",
+  },
+  icon: {
+    width: 60,
+    height: 60,
+  },
+  categoryText: {
+    fontWeight: "500",
+    marginTop: 7,
+    textAlign: "center",
+  },
+});
+
+export default Categories;
