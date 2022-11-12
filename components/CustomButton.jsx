9f19a0bc420142d05e091eeb2c8b84c0fb0992f8
VoyagerIV <58806671+VoyagerIV@users.noreply.github.com>
2022-11-13 00:40:05 +1100

    Add basic custom button

---
+++ components/CustomButton.jsx
@@ -0,0 +1,41 @@
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
+
+const CustomButton = ({ title, onPress, disabled }) => {
+    return (
+      <TouchableOpacity
+        style={disabled ? [styles.button, styles.disabled] : [styles.button, styles.enabled]}
+        onPress={() => {
+          if (!disabled) {
+            onPress();
+          }
+        }}
+        activeOpacity={0.8}
+      >
+        <Text style={styles.title}>{ title }</Text>
+      </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: "100%",
+    height: 43,
+    borderRadius: 10,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  enabled: {
+    backgroundColor: "#00CA90",
+  },
+  disabled: {
+    backgroundColor: "#B3CBC4",
+  },
+  title: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold"
+  }
+});
+
+export default CustomButton;
