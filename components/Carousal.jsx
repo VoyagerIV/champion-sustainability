b68620b72c710723e6907fc016f2a7b8eb1049b8
VoyagerIV <58806671+VoyagerIV@users.noreply.github.com>
2022-11-18 16:56:04 +1100

    Change cherry blossom image

--- components/Carousal.jsx
+++ components/Carousal.jsx
@@ -25,33 +25,61 @@ const MyCarousel = ({ data }) => {
         renderItem={({ item, index }) => (
           <TouchableOpacity
             style={styles.carousel}
-            onPress={() =>
-              navigation.navigate("Event", {
-                image:
-                  "https://bloximages.chicago2.vip.townnews.com/tdn.com/content/tncms/assets/v3/editorial/d/9a/d9aa590b-95ca-5760-aba1-5ab8a79d460c/58a229d3cc894.image.jpg?resize=1200%2C800",
-                title: item.t,
-                organiser: "Environment & Heritage",
-                date: "11/11/2022",
-                time: "10:00 AM",
-                location: "Bondi Beach NSW",
-                attendees: 25,
-                capacity: 50,
-                categories: [],
-                status: "",
-                description:
-                  "Trees soak up CO2 naturally, which helps to promote clean and healthy air for all of us. That’s not all though. With trees able to store CO2, they decrease the amount of CO2 in the atmosphere, while using it in the process of photosynthesis to produce sugar, provide energy, and release oxygen. Did you know that living near trees has been scientifically shown to improve our mental health? Having trees in your life is more than an aesthetic boost, they also help provide a natural boost to our cognitive and psychological health. Counsellor and psychologist, Amanda Woolveridge, believes trees can play a major role in mental health. So what are you waiting? Come join us in making our planet healthier and more sustainable.",
-                comments: [
-                  {
-                    poster: "johnsmith",
-                    posterImg:
-                      "https://thebluegrasssituation.com/wp-content/uploads/2020/08/re-perez-branding-for-the-people-square-headshot.jpg",
-                    rating: 5,
-                    comment:
-                      "Great event! Been with this team on three events so far and they're the best!",
-                  },
-                ],
-              })
-            }
+            onPress={() => {
+              if (item.t === "Cherry Blossom Planting") {
+                navigation.navigate("Event", {
+                  image:
+                    "https://www.cumberland.nsw.gov.au/sites/default/files/styles/16_9_mobile/public/2019-10/Cherry-Blossom-Festival-2of2-1282x600_3.jpg?h=be6aa2d1&itok=EU3Xt7xp",
+                  title: item.t,
+                  organiser: "Ben Marcus",
+                  date: "12/12/2022",
+                  time: "10:00 AM - 3:00 PM",
+                  location: "Auburn NSW 2144",
+                  attendees: 25,
+                  capacity: 51,
+                  categories: [],
+                  status: "",
+                  description:
+                    "A great experience for anyone who loves gardening.",
+                  comments: [
+                    {
+                      poster: "johnsmith",
+                      posterImg:
+                        "https://thebluegrasssituation.com/wp-content/uploads/2020/08/re-perez-branding-for-the-people-square-headshot.jpg",
+                      rating: 5,
+                      comment:
+                        "Great event! Been with this team on three events so far and they're the best!",
+                    },
+                  ],
+                });
+              } else {
+                navigation.navigate("Event", {
+                  image:
+                    "https://bloximages.chicago2.vip.townnews.com/tdn.com/content/tncms/assets/v3/editorial/d/9a/d9aa590b-95ca-5760-aba1-5ab8a79d460c/58a229d3cc894.image.jpg?resize=1200%2C800",
+                  title: item.t,
+                  organiser: "Environment & Heritage",
+                  date: "11/11/2022",
+                  time: "10:00 AM",
+                  location: "Bondi Beach NSW",
+                  attendees: 25,
+                  capacity: 50,
+                  categories: [],
+                  status: "",
+                  description:
+                    "Trees soak up CO2 naturally, which helps to promote clean and healthy air for all of us. That’s not all though. With trees able to store CO2, they decrease the amount of CO2 in the atmosphere, while using it in the process of photosynthesis to produce sugar, provide energy, and release oxygen. Did you know that living near trees has been scientifically shown to improve our mental health? Having trees in your life is more than an aesthetic boost, they also help provide a natural boost to our cognitive and psychological health. Counsellor and psychologist, Amanda Woolveridge, believes trees can play a major role in mental health. So what are you waiting? Come join us in making our planet healthier and more sustainable.",
+                  comments: [
+                    {
+                      poster: "johnsmith",
+                      posterImg:
+                        "https://thebluegrasssituation.com/wp-content/uploads/2020/08/re-perez-branding-for-the-people-square-headshot.jpg",
+                      rating: 5,
+                      comment:
+                        "Great event! Been with this team on three events so far and they're the best!",
+                    },
+                  ],
+                });
+              }
+            }}
           >
             <Image source={item.i} resizeMode="contain" style={styles.image} />
             <Text style={styles.text}>{item.t}</Text>
