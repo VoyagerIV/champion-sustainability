6b698ab2884a5b9922209c98673db1b72e3a038b
mk17 <Kheir.mirza@gmail.com>
2022-11-18 14:36:58 +1100

    fixed badges on android

--- screens/MyBadges.jsx
+++ screens/MyBadges.jsx
@@ -2,34 +2,30 @@
 import TopBar from "../components/TopBar";
 import React from 'react';
 import {
-    StyleSheet,
-    View,
-    Text,
-    Image,
-    Pressable,
-    ScrollView
-  } from "react-native";
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Pressable,
+  ScrollView
+} from "react-native";
 import BadgeWindow from "../components/BadgeWindow";
-  
-const MyBadges = ({}) => {
+
+const MyBadges = ({ }) => {
   return (
     <>
-      <TopBar
-        icon="https://img.icons8.com/ios/512/left.png"
-        title="My Badges">
-      </TopBar>
-      <View style={{flex: 1,marginTop:120}}>
+      <View style={{ flex: 1 }}>
         <ScrollView style={styles.scrollView}>
-        <BadgeWindow isShared={true}  imgSource={require('../assets/badge.png')} Title="Cleaned a Beach!" Date="11/11/2022 10AM-3:00PM" Caption="You have volunteered to clean the Tasman Sea!"/>
-        <BadgeWindow isShared={true}  imgSource={require('../assets/animalBadge.png')} Title="Took Care of Animals!" Date="27/10/2022 10AM-3:00PM" Caption="You have helped workers at Toranga Zoo take care of the animals! Good Job! "/>
-        <BadgeWindow isShared={false}  imgSource={require('../assets/rvBadge.png')} Title="Revived Volunteering!" Date="3/4/2022 10AM-3:00PM" Caption="You hosted an event to take care of the enviroment!"/>
-        <BadgeWindow isShared={false}  imgSource={require('../assets/joinedBadge.png')} Title="Joined Us!" Date="1/4/2022 6:00PM" Caption="You have joined Champion Sustainability!"/>
+          <BadgeWindow isShared={true} imgSource={require('../assets/badge.png')} Title="Cleaned a Beach!" Date="11/11/2022 10AM-3:00PM" Caption="You have volunteered to clean the Tasman Sea!" />
+          <BadgeWindow isShared={true} imgSource={require('../assets/animalBadge.png')} Title="Took Care of Animals!" Date="27/10/2022 10AM-3:00PM" Caption="You have helped workers at Toranga Zoo take care of the animals! Good Job! " />
+          <BadgeWindow isShared={false} imgSource={require('../assets/rvBadge.png')} Title="Revived Volunteering!" Date="3/4/2022 10AM-3:00PM" Caption="You hosted an event to take care of the enviroment!" />
+          <BadgeWindow isShared={false} imgSource={require('../assets/joinedBadge.png')} Title="Joined Us!" Date="1/4/2022 6:00PM" Caption="You have joined Champion Sustainability!" />
         </ScrollView>
       </View>
-    
+
     </>
-    
-    );
+
+  );
 }
 
 
@@ -48,9 +44,9 @@ const styles = StyleSheet.create({
   scrollView: {
     marginHorizontal: 20,
   },
-  
-  
-  });
+
+
+});
 export default MyBadges;
 
 
